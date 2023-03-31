8db9b004bcbc0a7cf219acbbc5b588004ca68de3
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2023-03-31 13:21:01 +0300

    2

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -64,7 +64,7 @@ class CategorySerializer(serializers.ModelSerializer):
 class TitleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Title
-        exclude = ('id', )
+        exclude = ('id',)
 
 
 class GenreSerializer(serializers.ModelSerializer):
