12cdcf1e5f49978f006a5ccf7aad4802955ded25
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2023-04-01 07:19:41 +0300

    2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,7 +7,8 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action, api_view
 from rest_framework.exceptions import ValidationError
 from rest_framework.filters import SearchFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 from reviews.models import Category, Genre, Review, Title, User
@@ -105,8 +106,32 @@ class TitleViewSet(viewsets.ModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    """Представление класса comment"""
     serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        """Набор объектов из базы данных."""
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        return title.comments
+
+    def perform_create(self, serializer):
+        """Сохранение нового комментария."""
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    """Представление класса review"""
     serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        """Набор объектов из базы данных."""
+        title = get_object_or_404(Title, pk=self.kwargs.get('review_id'))
+        return title.reviews
+
+    def perform_create(self, serializer):
+        """Сохранение нового отзыва."""
+        title = get_object_or_404(Title, pk=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, title=title)
