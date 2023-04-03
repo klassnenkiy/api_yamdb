1209feea68fb1893abe4fb6f782029f70f4b8f41
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2023-04-03 11:35:45 +0300

    60 / 77

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -3,13 +3,20 @@ from reviews.models import Category, Genre, Title
 
 
 class TitleFilter(django_filters.FilterSet):
-    category = django_filters.ModelChoiceFilter(field_name='category__slug',
-                                            to_field_name='category',
-                                            queryset=Category.objects.all())
-    genre = django_filters.ModelChoiceFilter(field_name='genre__slug',
-                                            to_field_name='genre',
-                                            queryset=Genre.objects.all())
+    category = django_filters.ModelChoiceFilter(
+        field_name='category__slug',
+        to_field_name='category',
+        queryset=Category.objects.all()
+    )
+    genre = django_filters.ModelChoiceFilter(
+        field_name='genre__slug',
+        to_field_name='genre',
+        queryset=Genre.objects.all()
+    )
 
     class Meta:
         model = Title
-        fields = ('category', 'genre')
+        fields = (
+            'category',
+            'genre'
+    )
