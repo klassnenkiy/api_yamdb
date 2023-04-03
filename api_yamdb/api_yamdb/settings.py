e7816c784b1e1d113d54eef16f1a3f314c385f30
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2023-04-03 21:39:05 +0300

    1st

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'import_export',
+    'import_export',
     'django_filters',
     'api',
     'reviews',
